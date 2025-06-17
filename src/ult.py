9c579637226237644ab97ec4b384e0e3851704b2
al-amin <kalamin957@gmail.com>
2025-06-17 10:43:52 +0100

    implement a func that adds secrets to secret manager

--- src/ult.py
+++ src/ult.py
@@ -1,2 +1,15 @@
+import json
+
+
+def creatingSecret(sm_client,name,userid,password):
+    # creates a secret 
+    secretValue = {
+        "userid": userid,
+        "password": password
+    }
+    secretString = json.dumps(secretValue)
+    return sm_client.create_secret(Name=name,SecretString=secretString)
+
+
 if __name__ == "__main__":
     pass
