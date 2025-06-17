4b7e23516b9e89e2adbbb23261eb1ce38e7876f3
al-amin <kalamin957@gmail.com>
2025-06-17 11:40:43 +0100

    implement a func that retrieve Secrets

--- src/ult.py
+++ src/ult.py
@@ -1,5 +1,5 @@
 import json
-
+import botocore.exceptions
 
 def creatingSecret(sm_client,name,userid,password):
     # creates a secret 
@@ -11,5 +11,21 @@ def creatingSecret(sm_client,name,userid,password):
     return sm_client.create_secret(Name=name,SecretString=secretString)
 
 
+def retrieveSecret(sm_client,title):
+    #given a title, returns the username and password into a json file
+    try:
+        response = sm_client.get_secret_value(SecretId=title)
+        passwords = json.loads(response["SecretString"])
+        with open("secrets.json", "w") as file:
+            json.dump(passwords,file,indent=4)
+
+        print(f"secrets is stored in secrets.json")
+        
+    except botocore.exceptions.ClientError as err:
+        print(f"Failed to retrieve secret: {err}")
+    except (json.JSONDecodeError, KeyError) as err:
+        print(f"Invalid secret format: {err}")
+    except Exception as err:
+        print(f"Unexpected error: {err}")
 if __name__ == "__main__":
     pass
